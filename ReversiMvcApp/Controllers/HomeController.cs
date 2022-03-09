ac6a0343f4150474b265c7ef979197807cd58db2
Ward Pieters <ward@wardpieters.nl>
2022-03-09 19:58:51 +0100

    Level 7-1

--- ReversiMvcApp/Controllers/HomeController.cs
+++ ReversiMvcApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ReversiMvcApp.Models;
@@ -23,6 +24,7 @@ namespace ReversiMvcApp.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
